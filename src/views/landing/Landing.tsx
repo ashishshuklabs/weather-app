9a9e7def08ac65cb187f98493644fade66f15dbc
Ashish Shukla <ashish.shukla@aderant.com>
2021-12-11 18:09:28 +1300

    added a button to fetch latest data on click

--- src/views/landing/Landing.tsx
+++ src/views/landing/Landing.tsx
@@ -5,16 +5,35 @@ import { SydneyCard } from "../../layout/card/SydneyCard";
 import { CanberraCard } from "../../layout/card/CanberraCard";
 import { GoldCoastCard } from "../../layout/card/GoldCoastCard";
 import { Header } from "../../layout/header/Header";
+import { OutlinedButton } from "../../components/form/button/OutlinedButton";
+import { useDispatch } from "react-redux";
+import { fetchCityWeatherData } from "../../state/action-creators/searchWeatherActionCreators";
+import { cityData } from "../../models/cityData";
 export const Landing = () => {
+  const dispatch = useDispatch();
+  const handleClick = () => {
+    dispatch(fetchCityWeatherData(cityData.melbourne));
+    dispatch(fetchCityWeatherData(cityData.brisbane));
+    dispatch(fetchCityWeatherData(cityData.sydney));
+    dispatch(fetchCityWeatherData(cityData.canberra));
+    dispatch(fetchCityWeatherData(cityData.goldCoast));
+  };
   return (
     <LandingWrapper>
       <Header />
+      <div className="button-wrapper">
+        <OutlinedButton
+          color="transparent"
+          hoverColor="#4a4a4a"
+          title="refresh data"
+          onClick={handleClick}
+        />
+      </div>
       <div className="cards-container">
         <MelbourneCard />
         <BrisbaneCard />
         <SydneyCard />
         <GoldCoastCard />
-
         <CanberraCard />
       </div>
     </LandingWrapper>
@@ -26,6 +45,13 @@ const LandingWrapper = styled.main`
   width: 100%;
   min-height: 100vh;
   background: linear-gradient(to bottom, #333, #707070);
+  display: flex;
+  flex-direction: column;
+  .button-wrapper {
+    max-width: 10rem;
+    align-self: center;
+    margin: 2rem 0;
+  }
   .cards-container {
     display: flex;
     flex-direction: column;
